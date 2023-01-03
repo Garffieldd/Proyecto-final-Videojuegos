95d45e71abccf22c60189b7535d856fc838e87ee
Sebastian <roncancio.juan@correounivalle.edu.co>
2023-01-03 00:19:27 -0500

    Implementacion vida y daño del jugador

--- src/scenes.js
+++ src/scenes.js
@@ -6,18 +6,25 @@ class Scene extends Phaser.Scene {
 
         // Variables del juego
         
-       
+       //player vars
         this.playerVelocity = 100;
         this.playerDiagVelocity = 69.4;
+        this.jumps = 0;  
+        this.dirX = 1;
+        this.dirY = 1; 
+        this.chill = 0;
+        this.takingDamage = 1;
+        this.healthState = this.chill;
+        this.damagedTime = 0;
+        //ogre vars
         this.ogreSpeed = 50;
-        //this.initial = 3;
         this.UP = 0;
         this.DOWN = 1;
         this.LEFT = 2;
         this.RIGHT = 3;
         this.initial = this.RIGHT;
-        this.randomMoveTime = 2000;
-        
+        this.randomMoveTime = 2000;   
+        this.health = 3;
     }
     
     // Carga de los assets
@@ -31,6 +38,10 @@ class Scene extends Phaser.Scene {
         this.load.image('tiles', 'assets/img/Tiles/0x72_DungeonTilesetII_v1.4.png')
         this.load.tilemapTiledJSON('dungeon', 'assets/img/Tiles/dungeon.json')
         this.load.image('katana','assets/img/weapon_katana.png' )
+        this.load.image('fullHeart','assets/img/fulled heart.png')
+        this.load.image('semifulledHeart','assets/img/semifulled heart.png')
+        this.load.image('emptyHeart','assets/img/empty heart.png')
+
         //Plugin para tiles animados
         this.load.scenePlugin('AnimatedTiles', 'https://raw.githubusercontent.com/nkholski/phaser-animated-tiles/master/dist/AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
         //Musica
@@ -52,6 +63,9 @@ class Scene extends Phaser.Scene {
         
         wallsLayer.setCollisionByProperty({collide: true})
         this.animatedTiles.init(map)
+
+
+        
         
         /*
         const debugGraphics = this.add.graphics().setAlpha(0.7)
@@ -68,20 +82,27 @@ class Scene extends Phaser.Scene {
         this.player.setSize(11,15,true);
         this.player.setOffset(3,9);
         this.physics.add.collider(this.player, wallsLayer);
-        this.cameras.main.startFollow(this.player,true);
+        this.cameras.main.startFollow(this.player, true);
 
         this.KeyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.KeyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.KeyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.KeyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+        //Generar Katana
+        this.katana = this.physics.add.sprite(170,80,'katana');
+        this.katana.setScale(0.5);
+        this.katana.setSize(5,8);
+
         //Contenedor
         /*
-       this.container = this.add.container(this.player)
-       this.container.setSize(11,15)
-       this.physics.world.enable(this.container)
-       this.container.add(this.player)
-        */
+       this.contenedor = this.add.container(170,80)
+       this.contenedor.add(this.player);
+       this.contenedor.add(this.katana);
+       this.cameras.main.startFollow(this.contenedor, true);
+    */
+       //console.log(this.contenedor);
+      
        
             
         this.anims.create({
@@ -110,8 +131,13 @@ class Scene extends Phaser.Scene {
         this.newOgre();
         this.physics.add.collider(this.ogre, wallsLayer, () => {
             this.initial = randomDirection(this.initial);
-            console.log('estoy funcionando')
+            //console.log('estoy funcionando')
         });
+
+        this.physics.add.collider(this.ogre, this.player, this.collideWithOgre, undefined, this);
+        
+        this.UIlife();
+        
 
         this.time.addEvent({
             delay: this.randomMoveTime,
@@ -122,6 +148,41 @@ class Scene extends Phaser.Scene {
         })
         
     };
+
+    
+    
+    collideWithOgre(ogre,player){
+         //console.log(ogre)
+         //console.log(player)
+
+        const xDirection = player.x - ogre.x;
+        const yDirection = player.y - ogre.y;
+
+        const newDirection = new Phaser.Math.Vector2(xDirection,yDirection).normalize().scale(250);
+
+        this.health--;
+
+        if (this.health === 0){
+            this.scene.start('GameOver')
+            console.log('Game Over')
+        }
+
+        if(this.healthState === this.takingDamage){
+            return
+        }
+
+        
+        player.setVelocity(newDirection.x,newDirection.y);
+        player.setTint(0xff0000);
+        this.damagedTime = 0
+        this.healthState = this.takingDamage;
+        this.healthHandler();
+        console.log(this.health);
+        
+        
+    }
+
+
 
     newOgre(){
          //Generar enemigo
@@ -147,73 +208,158 @@ class Scene extends Phaser.Scene {
 
     }
 
+    UIlife(){
+        this.hearts = this.add.group()
+
+        this.hearts.createMultiple({
+            key: 'fullHeart',
+            setXY: {
+                x: -10,
+                y: -30,
+                stepX: 16
+            },
+            quantity: 3
+        })
+        //this.healthHandler();
+    }
+
+    
+    healthHandler(){
+        this.hearts.children.each((gameObject,index) => {
+            const heart = gameObject
+            if (index < this.health){
+                console.log(index);
+                console.log(this.health);
+                heart.setTexture('fullHeart')
+            }else{
+                heart.setTexture('emptyHeart')
+            }
+        })
+        
+    }
+
    
 
     
 // Actualizacion de los elementos del videojuego
  update(time,deltatime)
     {
+
+         //Animacion de daño
+         
+    switch (this.healthState){
+        case this.chill:
+            //console.log('tamos de chill')
+            break
+        
+        case this.takingDamage:
+            
+            this.damagedTime += deltatime
+            if (this.damagedTime >= 300){
+                this.healthState = this.chill;
+                this.player.clearTint();
+                this.damagedTime = 0;
+                //console.log('ay mi piernita')
+            }
+            break
+    }
+
+    if (this.healthState === this.takingDamage){
+        return
+    }
+
+        
+        
         //Controlador del Jugador
+       // const velocityVector = new Phaser.Math.Vector2(this.dirX,this.dirY).normalize() * this.playerVelocity;
+       const velocityVector = new Phaser.Math.Vector2(this.playerVelocity * this.dirX,this.playerVelocity * this.dirY).normalize().scale(200);
+
     if(!this.KeyW.isDown && !this.KeyA.isDown && !this.KeyS.isDown && !this.KeyD.isDown){    
-    this.player.setVelocityX(0);
-    this.player.setVelocityY(0);
-    this.player.play('stopped',true);
+        this.player.setVelocity(0,0);
+        this.player.play('stopped',true);
     }
     if(this.KeyW.isDown) {
-        console.log('W key pressed')
-        this.player.setVelocityY(-this.playerVelocity);
+        //console.log('W key pressed')
+        this.dirY = -1;
+        this.player.setVelocity(0,velocityVector.y);
+        
         this.player.play('walk',true);
+        
     } 
 
     if(this.KeyA.isDown) {
-        console.log('A key pressed')
+        //console.log('A key pressed')
+        this.dirX = -1;
         this.player.flipX = true;
-        this.player.setVelocityX(-this.playerVelocity);
+        this.player.setVelocity(velocityVector.x,0);
         this.player.play('walk',true);
+        
     } 
     if(this.KeyS.isDown) {
-        console.log('S key pressed')
-        this.player.setVelocityY(this.playerVelocity);
+        //console.log('S key pressed')
+        this.dirY = 1;
+        this.player.setVelocity(0,velocityVector.y);
         this.player.play('walk',true);
+        
     } 
     if(this.KeyD.isDown) {
-        console.log('D key pressed')
+        //console.log('D key pressed')
+        this.dirX = 1;
         this.player.flipX = false;
-        this.player.setVelocityX(this.playerVelocity);
+        this.player.setVelocity(velocityVector.x,0);
         this.player.play('walk',true);
+        
     }
+
+    
     // Diagonal movement
     // Up and left
+
+    const velocityDiagVector = new Phaser.Math.Vector2(this.playerDiagVelocity * this.dirX,this.playerDiagVelocity * this.dirY).normalize().scale(100);
+    
     if (this.KeyA.isDown && this.KeyW.isDown)
 {
-    this.player.body.setVelocityX(-this.playerDiagVelocity);
-    this.player.body.setVelocityY(-this.playerDiagVelocity);
+    this.dirX = -1
+    this.dirY = -1
+    this.player.body.setVelocity(velocityDiagVector.x,velocityDiagVector.y);
     this.player.play('walk',true);
+    
+    //console.log('AW PRESSED')
 }
 
     // Up and right
     if (this.KeyD.isDown && this.KeyW.isDown)
     {
-        this.player.body.setVelocityX(this.playerDiagVelocity);
-        this.player.body.setVelocityY(-this.playerDiagVelocity);
+        this.dirX = 1
+        this.dirY = -1
+        this.player.body.setVelocity(velocityDiagVector.x,velocityDiagVector.y);
         this.player.play('walk',true);
+        //console.log('DW PRESSED')
     }
 
     // Down and right
     if (this.KeyD.isDown && this.KeyS.isDown)
     {
-        this.player.body.setVelocityX(this.playerDiagVelocity);
-        this.player.body.setVelocityY(this.playerDiagVelocity);
+
+        this.dirX = 1
+        this.dirY = 1
+        this.player.body.setVelocity(velocityDiagVector.x,velocityDiagVector.y);
         this.player.play('walk',true);
+        //console.log('DS PRESSED')
     }
 
     // Down and left
+    
     if (this.KeyA.isDown && this.KeyS.isDown)
     {
-        this.player.body.setVelocityX(-this.playerDiagVelocity);
-        this.player.body.setVelocityY(this.playerDiagVelocity);
+        this.dirX = -1
+        this.dirY = 1
+        this.player.body.setVelocity(velocityDiagVector.x,velocityDiagVector.y);
         this.player.play('walk',true);
+        //console.log('AS PRESSED')
     }
+    
+    
 
     // IA del ogro
 
@@ -236,7 +382,14 @@ class Scene extends Phaser.Scene {
         }
 
 
+
+
+       
     }
+
+    
+
+    
 }
 
 class FinalScene extends Phaser.Scene {
